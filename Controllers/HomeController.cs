d2713796993adbb9f6bc8e276043d9087945bc55
admin <admin@DESKTOP-A4VA8VB>
2024-09-24 09:29:51 +0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace App_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult trangchu()
+        {
+            return View();
+        }
+
+        public ActionResult gioithieu()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult lienhe()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
